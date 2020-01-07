4d78c3e8e9c7046f94e2eeacbf7af7db938a4d3b
Quintin Herb <qherb@ycp.edu>
2020-01-07 16:47:40 -0500

    Dropdown adjustments

--- scripts/expand-project.js
+++ scripts/expand-project.js
@@ -3,12 +3,15 @@ let active = null;
 function expandDescription(project) {
     if (active != project) {
         if (active != null) {
-            document.getElementById(active).classList.remove('active');
+            document.getElementById(active).style.maxHeight = "0px";
+            document.getElementById(active).style.padding = "0px 14px";
         }
-        document.getElementById(project).classList.add('active');
+        document.getElementById(project).style.maxHeight = "200px";
+        document.getElementById(project).style.padding = "14px";
         active = project;
     } else {
-        document.getElementById(project).classList.remove('active');
+        document.getElementById(project).style.maxHeight = "0px";
+        document.getElementById(project).style.padding = "0px 14px";
         active = null;
     }
 }
