98c5a845d540024e9065b1e4a6eb0ccba0ac8d1b
Quintanamo <qherb@ycp.edu>
2022-04-14 19:11:40 -0400

    Added snake to applications

--- Quindows95/js/applications.js
+++ Quindows95/js/applications.js
@@ -19,3 +19,23 @@ applications["about-me"] = {
     canResize: false,
     disableScroll: true
 };
+
+applications["snake"] = {
+    name: "Checkbox Snake",
+    title: "Checkbox Snake",
+    content: "templates/snake.html",
+    defaultWidth: "600px",
+    defaultHeight: "600px",
+    canResize: true,
+    disableScroll: false
+};
+
+applications["linkedin"] = {
+    name: "Quintin Herb - LinkedIn",
+    title: "Quintin Herb - LinkedIn",
+    content: "https://www.linkedin.com/in/quintinherb",
+    defaultWidth: "600px",
+    defaultHeight: "600px",
+    canResize: true,
+    disableScroll: false
+};
